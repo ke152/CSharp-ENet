6f6bbdc88bfacafa91d375f9152a65c518bb62bf
ke152 <ke152@foxmail.com>
2022-07-02 15:21:07 +0800

    Update：variant

--- CSharpENet/CSharpENet/Host.cs
+++ CSharpENet/CSharpENet/Host.cs
@@ -5,37 +5,35 @@ class ENetHost : Singleton<ENetHost>
 {
     // public ENetSocket socket;
     // public ENetAddress address;                     /**< Internet address of the host */
-    // public enet_uint32 incomingBandwidth;           /**< downstream bandwidth of the host */
-    // public enet_uint32 outgoingBandwidth;           /**< upstream bandwidth of the host */
-    // public enet_uint32 bandwidthThrottleEpoch;
-    // public enet_uint32 mtu;
-    // public enet_uint32 randomSeed;
-    // public int recalculateBandwidthLimits;
-    // public ENetPeer* peers;                       /**< array of peers allocated for this host */
-    // public size_t peerCount;                   /**< number of peers allocated for this host */
-    // public size_t channelLimit;                /**< maximum number of channels allowed for connected peers */
-    // public enet_uint32 serviceTime;
+    public uint incomingBandwidth;           /**< downstream bandwidth of the host */
+    public uint outgoingBandwidth;           /**< upstream bandwidth of the host */
+    public uint bandwidthThrottleEpoch;
+    public uint mtu;
+    public uint randomSeed;
+    public int recalculateBandwidthLimits;
+    public List<ENetPeer> peers = new List<ENetPeer>();                       /**< array of peers allocated for this host */
+    public uint peerCount;                   /**< number of peers allocated for this host */
+    public uint channelLimit;                /**< maximum number of channels allowed for connected peers */
+    public uint serviceTime;
     public LinkedList<ENetPeer> dispatchQueue = new LinkedList<ENetPeer>();
-    // public int continueSending;
-    // public size_t packetSize;
-    // public enet_uint16 headerFlags;
-    // public ENetProtocol commands[ENET_PROTOCOL_MAXIMUM_PACKET_COMMANDS];
-    // public size_t commandCount;
-    // public ENetBuffer buffers[ENET_BUFFER_MAXIMUM];
-    // public size_t bufferCount;
-    // public ENetCompressor compressor;
-    // public enet_uint8 packetData[2][ENET_PROTOCOL_MAXIMUM_MTU];
+    public int continueSending;
+    public uint packetSize;
+    public uint headerFlags;
+    public ENetProto[] commands = new ENetProto[ENetDef.ProtoMaxPacketCmds];
+    public uint commandCount;
+    public byte[] buffers = new byte[ENetDef.BufferMax];
+    public uint bufferCount;
+    public byte[,] packetData = new byte[2,ENetDef.ProtoMaxMTU];
     //public ENetAddress receivedAddress;
-    // public enet_uint8* receivedData;
-    // public size_t receivedDataLength;
-    // public enet_uint32 totalSentData;               /**< total data sent, user should reset to 0 as needed to prevent overflow */
-    // public enet_uint32 totalSentPackets;            /**< total UDP packets sent, user should reset to 0 as needed to prevent overflow */
-    // public enet_uint32 totalReceivedData;           /**< total data received, user should reset to 0 as needed to prevent overflow */
-    // public enet_uint32 totalReceivedPackets;        /**< total UDP packets received, user should reset to 0 as needed to prevent overflow */
-    // public ENetInterceptCallback intercept;                  /**< callback the user can set to intercept received raw UDP packets */
-    // public size_t connectedPeers;
-    // public size_t bandwidthLimitedPeers;
-    // public size_t duplicatePeers;              /**< optional number of allowed peers from duplicate IPs, defaults to ENET_PROTOCOL_MAXIMUM_PEER_ID */
+    public byte[]? receivedData;
+    public uint receivedDataLength;
+    public uint totalSentData;               /**< total data sent, user should reset to 0 as needed to prevent overflow */
+    public uint totalSentPackets;            /**< total UDP packets sent, user should reset to 0 as needed to prevent overflow */
+    public uint totalReceivedData;           /**< total data received, user should reset to 0 as needed to prevent overflow */
+    public uint totalReceivedPackets;        /**< total UDP packets received, user should reset to 0 as needed to prevent overflow */
+    public uint connectedPeers;
+    public uint bandwidthLimitedPeers;
+    public uint duplicatePeers;              /**< optional number of allowed peers from duplicate IPs, defaults to ENET_PROTOCOL_MAXIMUM_PEER_ID */
     public uint maximumPacketSize;           /**< the maximum allowable packet size that may be sent or received on a peer */
     public uint maximumWaitingData;          /**< the maximum aggregate amount of buffer space a peer may use waiting for packets to be delivered */
 }
