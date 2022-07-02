7dbea83a54f9daf0d817098350d729dd094a0f0c
ke152 <ke152@foxmail.com>
2022-07-02 23:07:41 +0800

    Update：name

--- CSharpENet/CSharpENet/Channel.cs
+++ CSharpENet/CSharpENet/Channel.cs
@@ -1,15 +1,15 @@
 ﻿namespace ENet;
 
-struct ENetChannel
+class ENetChannel
 {
-    public uint outReliableSeqNumber = 0;
-    public uint outUnreliableSeqNum = 0;
-    public uint usedReliableWindows = 0;
-    public uint[] rSeliableWindows = new uint[(int)ENetDef.PeerReliableWindows];
-    public uint inReliableSeqNum = 0;
-    public uint inUnreliableSeqNum = 0;
-    public LinkedList<ENetInCmd> inReliableCmds = new();
-    public LinkedList<ENetInCmd> inUnreliableCmds = new();
+    public uint outgoingReliableSequenceNumber = 0;
+    public uint outgoingUnreliableSequenceNumber = 0;
+    public int usedReliableWindows = 0;
+    public uint[] reliableWindows = new uint[(int)ENetDef.PeerReliableWindows];
+    public uint incomingReliableSequenceNumber = 0;
+    public uint incomingUnreliableSequenceNumber = 0;
+    public LinkedList<ENetInCmd> incomingReliableCommands = new();
+    public LinkedList<ENetInCmd> incomingUnreliableCommands = new();
 
     public ENetChannel()
     {
