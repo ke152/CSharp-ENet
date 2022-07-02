e192f6dab88e90c85063e7448e7dbf1e6a16c0d4
ke152 <ke152@foxmail.com>
2022-07-02 16:44:59 +0800

    New file

---
+++ CSharpENet/CSharpENet/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENet
+{
+    internal class ENetEvent
+    {
+        public ENetEventType type = ENetEventType.None;      /**< type of the event */
+        public ENetPeer? peer;      /**< peer that generated a connect, disconnect or receive event */
+        public uint channelID; /**< channel on the peer that generated the event, if appropriate */
+        public uint data;      /**< data associated with the event, if appropriate */
+        public ENetPacket? packet;    /**< packet associated with the event, if appropriate */
+    }
+
+    enum ENetEventType
+    {
+        None = 0,
+        Connect = 1,
+        Disconnect = 2,
+        Recv = 3
+    }
+}
